775ae91ebd15fff8bc40f33db59a136460c0a8ec
Mandy-cyber <acm.rodriques@gmail.com>
2023-07-15 05:26:34 -0400

    creating enums for Content class

---
+++ src/main/java/textEditor/model/enums/ContentType.java
@@ -0,0 +1,61 @@
+package textEditor.model.enums;
+
+/**
+ * Represents a type of HTML content that can be used in the text editor
+ */
+public enum ContentType {
+    /**
+     * Equivalent of html 'h1' tag
+     */
+    H1,
+
+    /**
+     * Equivalent of html 'h2' tag
+     */
+    H2,
+
+    /**
+     * Equivalent of html 'h3' tag
+     */
+    H3,
+
+    /**
+     * Equivalent of html 'h4' tag
+     */
+    H4,
+
+    /**
+     * Equivalent of html 'h5' tag
+     */
+    H5,
+
+    /**
+     * Equivalent of html 'h6' tag
+     */
+    H6,
+
+    /**
+     * Equivalent of html 'p' tag
+     */
+    PARA,
+
+    /**
+     * Equivalent of html  'strong' tag
+     */
+    BOLD,
+
+    /**
+     * Equivalent of html  'em' tag
+     */
+    ITALIC,
+
+    /**
+     * Equivalent of html  'u' tag
+     */
+    UNDERLINE,
+
+    /**
+     * Equivalent of html 'link' tag
+     */
+    STYLE_LINK
+}
---
+++ src/main/java/textEditor/model/enums/StyleType.java
@@ -0,0 +1,26 @@
+package textEditor.model.enums;
+
+/**
+ * Represents a style that an element can adopt
+ */
+public enum StyleType {
+    /**
+     * The size of a font
+     */
+    FONT_SIZE,
+
+    /**
+     * The color of a font
+     */
+    FONT_COLOR,
+
+    /**
+     * The font style/family of a font
+     */
+    FONT_STYLE,
+
+    /**
+     * The background color of an element
+     */
+    BG_COLOR
+}
