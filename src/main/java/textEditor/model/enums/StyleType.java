18d07c481bf3edb123d847f3de45edeb0e349204
Mandy-cyber <acm.rodriques@gmail.com>
2023-07-15 18:01:29 -0400

    adding attrs to style types

--- src/main/java/textEditor/model/enums/StyleType.java
+++ src/main/java/textEditor/model/enums/StyleType.java
@@ -1,26 +1,46 @@
 package textEditor.model.enums;
 
 /**
- * Represents a style that an element can adopt
+ * Represents a css style that an element can adopt
  */
 public enum StyleType {
     /**
      * The size of a font
      */
-    FONT_SIZE,
+    FONT_SIZE("font-size:"),
 
     /**
      * The color of a font
      */
-    FONT_COLOR,
+    FONT_COLOR("color:"),
 
     /**
      * The font style/family of a font
      */
-    FONT_STYLE,
+    FONT_STYLE("font-family:"),
 
     /**
      * The background color of an element
      */
-    BG_COLOR
+    BG_COLOR("background-color:");
+
+    private final String attr;
+
+    /**
+     * Initializes a StyleType with the given attr
+     *
+     * @param attr the attribute for the style element
+     */
+    StyleType(String attr) {
+        this.attr = attr;
+    }
+
+    /**
+     * Gets the attribute of this style type
+     *
+     * @return the style's attribute
+     */
+    public String attr() {
+        return this.attr;
+    }
 }
