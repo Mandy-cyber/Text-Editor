b732cce490209a0bc7d2746c2c8e301909f0b3c4
Mandy-cyber <acm.rodriques@gmail.com>
2023-07-15 19:00:08 -0400

    writing write() method

--- src/main/java/textEditor/model/io/FileWriter.java
+++ src/main/java/textEditor/model/io/FileWriter.java
@@ -1,4 +1,19 @@
 package textEditor.model.io;
 
+import java.io.File;
+
+/**
+ * Represents a tool for writing to a file
+ */
 public interface FileWriter {
+
+    /**
+     * Writes the given content to the given html file
+     *
+     * @param content the string content to be written
+     * @param file the file to write to
+     * @throws IllegalArgumentException if the given file is not of the correct extension
+     * @throws RuntimeException if the file could not be found or written to
+     */
+    void write(String content, File file);
 }
--- src/main/java/textEditor/model/io/HtmlWriter.java
+++ src/main/java/textEditor/model/io/HtmlWriter.java
@@ -1,4 +1,36 @@
 package textEditor.model.io;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Represents a tool for writing to a html file
+ */
 public class HtmlWriter implements FileWriter {
+    /**
+     * Writes the given html content to the given html file
+     *
+     * @param content the string of html content to be written
+     * @param file the html file to write to
+     * @throws IllegalArgumentException if the given file is not a html file
+     * @throws RuntimeException if the given file could not be found or written to
+     */
+    @Override
+    public void write(String content, File file) {
+        // is not a html file
+        if (!file.getName().endsWith(".html")) {
+            throw new IllegalArgumentException("Cannot write html to a non-html file");
+        }
+
+        // is a html file
+        Path p = file.toPath();
+        byte[] data = content.getBytes();
+        try {
+            Files.write(p, data);
+        } catch (Exception e) {
+            throw new RuntimeException("The given file could not be found or written to");
+        }
+    }
+
 }
