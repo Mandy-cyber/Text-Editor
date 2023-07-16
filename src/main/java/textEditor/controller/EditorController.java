74ed8b2218274b05c4aa4651b3f1d2c7455a3638
Mandy-cyber <acm.rodriques@gmail.com>
2023-07-15 20:10:46 -0400

    laying out classes

--- src/main/java/textEditor/controller/EditorController.java
+++ src/main/java/textEditor/controller/EditorController.java
@@ -1,4 +1,36 @@
 package textEditor.controller;
 
+import javafx.stage.Stage;
+import textEditor.model.PageFile;
+
+/**
+ * Represents a tool for controlling the display and functionality of
+ * the 'Editor' page in the Text Editor app
+ */
 public class EditorController extends PageController {
+    /**
+     * Initializes a new EditorController with the given page on the given stage
+     *
+     * @param pageFile a page file
+     * @param stage the stage that will be loaded
+     */
+    public EditorController(PageFile pageFile, Stage stage) {
+        super(pageFile, stage);
+    }
+
+    /**
+     * Runs the controller
+     */
+    @Override
+    public void run() {
+
+    }
+
+    /**
+     * Initializes the elements on the page
+     */
+    @Override
+    public void initElements() {
+
+    }
 }
--- src/main/java/textEditor/controller/SplashController.java
+++ src/main/java/textEditor/controller/SplashController.java
@@ -1,4 +1,36 @@
 package textEditor.controller;
 
+import javafx.stage.Stage;
+import textEditor.model.PageFile;
+
+/**
+ * Represents a tool for controlling the display and functionality of
+ * the 'Splash' screen page in the Text Editor app
+ */
 public class SplashController extends PageController {
+    /**
+     * Initializes a new SplashController with the given page on the given stage
+     *
+     * @param pageFile a page file
+     * @param stage the stage that will be loaded
+     */
+    public SplashController(PageFile pageFile, Stage stage) {
+        super(pageFile, stage);
+    }
+
+    /**
+     * Runs the controller
+     */
+    @Override
+    public void run() {
+
+    }
+
+    /**
+     * Initializes the elements on the page
+     */
+    @Override
+    public void initElements() {
+
+    }
 }
